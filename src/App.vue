98a1df16abfdf9627b6a15b9a5c95e3a429eba50
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-30 20:13:02 -0300

    Add typography definitions

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,22 @@
   html
   body
     height: 100%
+
+  @font-face
+    font-family: 'Nexa'
+    src: url('~@/assets/fonts/Nexa-Light.woff') format('woff'),
+         url('~@/assets/fonts/Nexa-Light.woff2') format('woff2')
+    font-weight: 300
+
+  @font-face
+    font-family: 'Nexa'
+    src: url('~@/assets/fonts/Nexa-Bold.woff') format('woff'),
+         url('~@/assets/fonts/Nexa-Bold.woff2') format('woff2')
+    font-weight: 700
+
+  @font-face
+    font-family: 'Aristotelica Display'
+    src: url('~@/assets/fonts/AristotelicaDisplay-Extralight.woff') format('woff'),
+         url('~@/assets/fonts/AristotelicaDisplay-Extralight.woff2') format('woff2')
+    font-weight: 200
 </style>
