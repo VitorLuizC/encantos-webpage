a397fc9385628d194662e4624e9027a5232e786a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-30 18:46:56 -0300

    Move to Vue + POI Application

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Encantos</h1>
+</template>
+
+<style lang="stylus">
+  *
+    box-sizing: border-box
+    margin: 0
+    padding: 0
+
+  html
+    font-size: 62.5%
+
+  html
+  body
+    height: 100%
+</style>
